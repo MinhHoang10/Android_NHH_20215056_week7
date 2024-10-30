5fd8f519418db0933e9785e1a20623e649eed2df
Hoang <hoangr271@gmail.com>
2024-10-30 17:48:59 +0700

    Bai tuan 7

---
+++ settings.gradle.kts
@@ -0,0 +1,28 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+<<<<<<< HEAD
+rootProject.name = "TH_Android"
+=======
+rootProject.name = "Bai_Tren_Lop_Bai_2"
+>>>>>>> 61661a3 (bai_tren_lop_bai_2)
+include(":app")
+ 
